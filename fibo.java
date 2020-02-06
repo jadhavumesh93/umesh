8c77f1badaf8a5d46e9137e2caea1ecd3db523b9
jadhavumesh93 <60729348+jadhavumesh93@users.noreply.github.com>
2020-02-06 12:24:04 +0530

    fibo.java

---
+++ fibo.java
@@ -0,0 +1,17 @@
+imort java.io.*;
+class Fibo
+{
+ public static void main(String args[])
+ {
+  int a=0, b=1,c;
+  Console c = System.in();
+  for(int i=0; i<n; i++)
+  {
+   System.out.print(a+" ");
+   c = a+b;
+   a=b;
+   b=c;
+  }
+ }
+}
+   
